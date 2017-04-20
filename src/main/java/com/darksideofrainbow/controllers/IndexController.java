22357851f22826b34569f02eb528107a05267004
Jay <improcuziam@gmail.com>
2017-04-20 12:39:59 -0400

    Search and stuff

--- src/main/java/com/darksideofrainbow/controllers/IndexController.java
+++ src/main/java/com/darksideofrainbow/controllers/IndexController.java
@@ -1,5 +1,6 @@
 package com.darksideofrainbow.controllers;
 
+import com.darksideofrainbow.models.Album;
 import com.darksideofrainbow.models.ApplicationUser;
 import com.darksideofrainbow.service.AlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
@@ -24,5 +27,25 @@ public class IndexController {
     public String homePage(Model model, ApplicationUser user) {
         model.addAttribute("albums", albumService.findAll());
         return "index";
+    }
+
+    @RequestMapping(value = {"/search"}, method = RequestMethod.GET)
+    public String searchPage(Model model) {
+        return "search";
+    }
+
+    @RequestMapping(value = {"/search"}, method = RequestMethod.POST)
+    public String searchPostPage(Model model, @RequestParam(value = "searchquery") String query) {
+        List<Album> foundList = albumService.findByName(query);
+        model.addAttribute("query", query);
+
+        if(!foundList.isEmpty()) {
+            model.addAttribute("totFound", foundList.size());
+            model.addAttribute("isFound", true);
+            model.addAttribute("foundAlbums", foundList);
+        } else {
+            model.addAttribute("isFound", false);
+        }
+        return "search";
     }
 }
